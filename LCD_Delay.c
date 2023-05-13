c2245689d9a058570e4b9bef3499586068ddcf81
AhmedSamhan02 <133347755+AhmedSamhan02@users.noreply.github.com>
2023-05-13 22:49:55 +0300

    Update LCD_Delay.c

--- LCD_Delay.c
+++ LCD_Delay.c
@@ -1,1 +1,35 @@
-.
+
+#include "main.h"
+/**
+ * Delays execution for the specified number of milliseconds.
+ *
+ * @param n The number of milliseconds to delay.
+ */
+void delayMs(int n)
+{
+    volatile int i, j; // Volatile is important for variables incremented in code
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < 3180; j++) // Delay for 1 msec
+        {
+        }
+    }
+}
+
+/**
+ * Delays execution for the specified number of microseconds.
+ *
+ * @param n The number of microseconds to delay.
+ */
+void delayUs(int n)
+{
+    volatile int i, j; // Volatile is important for variables incremented in code
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < 3; j++) // Delay for 1 microsecond
+        {
+        }
+    }
+}
