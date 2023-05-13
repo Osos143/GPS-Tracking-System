702f3d25f61e85be8a076809459a94975bd61238
AhmedSamhan02 <133347755+AhmedSamhan02@users.noreply.github.com>
2023-05-13 22:50:22 +0300

    Update LCD_init.c

--- LCD_init.c
+++ LCD_init.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * Initializes the LCD in 4-bit mode.
+ */
+void LCD4bits_Init(void)
+{
+    SYSCTL_RCGCGPIO_R |= 0x02;   // Enable clock for PORTB
+    delayMs(10);                 // Delay 10 ms for enable the clock of PORTB
+    GPIO_PORTB_DIR_R = 0xFF;     // Set PORTB as output pins
+    GPIO_PORTB_DEN_R = 0xFF;     // Enable PORTB digital IO pins
+
+    // Initialize the LCD
+    LCD4bits_Cmd(0x28);          // 2 lines and 5x7 character (4-bit data, D4 to D7)
+    LCD4bits_Cmd(0x06);          // Automatic Increment cursor (shift cursor to right)
+    LCD4bits_Cmd(0x01);          // Clear display screen
+    LCD4bits_Cmd(0x0F);          // Display on, cursor blinking
+}
